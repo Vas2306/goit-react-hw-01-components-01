9a9812be9c2bf2b889275fdacd0935181005f186
Vas2306 <vasilisa230679@gmail.com>
2023-03-22 20:59:32 +0100

    task-04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile/Profile'
 import Statistics from './Statistics/Statistics';
+import TransactionHistory from './TransactionHistory/TransactionHistory'
 export const App = () => {
   return (
     <div className="App">
       <Profile />
-      <Statistics/>
+      <Statistics />
+      <TransactionHistory/>
     </div>
   );
 };
