dc9b668b131c97a6ecedc67b25cf7159a38ed986
Vas2306 <vasilisa230679@gmail.com>
2023-03-23 15:17:12 +0100

    task-03

---
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import s from "./FriendListItem.module.css";
+
+function FriendListItem({ isOnline, avatar, name }) {
+  return (
+    <li className={s.item}>
+      <span className={isOnline ? s.statusTrue : s.statusFalse}></span>
+      <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={s.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.prototype = {
+  isOnline: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default FriendListItem;
